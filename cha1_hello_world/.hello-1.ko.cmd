c384ac95fa5bdb009ac800bf325feb7a6242a5e9
Ubuntu <ubuntu@ip-172-31-40-95.us-west-2.compute.internal>
2014-10-06 05:02:50 +0000

    hello world

---
+++ cha1_hello_world/.hello-1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ubuntu/kmod/cha1_hello_world/hello-1.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-29-generic/scripts/module-common.lds --build-id  -o /home/ubuntu/kmod/cha1_hello_world/hello-1.ko /home/ubuntu/kmod/cha1_hello_world/hello-1.o /home/ubuntu/kmod/cha1_hello_world/hello-1.mod.o
---
+++ cha1_hello_world/.tmp_versions/hello-1.mod
@@ -0,0 +1,2 @@
+/home/ubuntu/kmod/cha1_hello_world/hello-1.ko
+/home/ubuntu/kmod/cha1_hello_world/hello-1.o
